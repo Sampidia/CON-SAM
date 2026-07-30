36b2b0f70c19934a92abb4e60ff5faad9b1b1016
Sampidia <okopidia@gmail.com>
2026-07-30 18:36:34 +0100

    Fix Smarty template permissions and enable debug mode

--- connect.sampidia.com/includes/config.php
+++ connect.sampidia.com/includes/config.php
@@ -9,8 +9,8 @@ define("DB_PASSWORD", getenv('DB_PASSWORD') ?: '');
 define("DB_HOST", getenv('DB_HOST') ?: 'localhost');
 define("DB_PORT", getenv('DB_PORT') ?: '3306');
 
-define("SYS_URL", getenv('SYS_URL') ?: 'http://localhost');
-define("DEBUGGING", getenv('DEBUGGING') === 'true');
+define("SYS_URL", rtrim(getenv('SYS_URL') ?: 'https://connect.sampidia.com', '/'));
+define("DEBUGGING", getenv('DEBUGGING') === 'false' ? false : true);
 define("DEFAULT_LOCALE", getenv('DEFAULT_LOCALE') ?: 'en_us');
 
 define("LICENCE_KEY", getenv('LICENCE_KEY') ?: 'xxxxxxxx');
